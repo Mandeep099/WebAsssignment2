ab06944adc26e169abd11213be748da763873b9a
Mandeep Bal <mbal099@mtroyal.ca>
2023-11-28 22:21:12 -0700

    Mandeep started to display the songs from the api in a table, may change display type later, created the methods for local storage, created a search box for titles and dropdown menus for artists and genres and populated the dropdown menu. Created the clear and filter buttons but currently do nothing. Finally created the stylesheet and added some details

--- assign2.js
+++ assign2.js
@@ -1,7 +1,7 @@
 
 
 /* url of song api --- https versions hopefully a little later this semester */	
-const api = 'http://www.randyconnolly.com/funwebdev/3rd/api/music/songs-nested.php';
+const api = 'https://www.randyconnolly.com/funwebdev/3rd/api/music/songs-nested.php';
 
  
 
@@ -14,81 +14,121 @@ const api = 'http://www.randyconnolly.com/funwebdev/3rd/api/music/songs-nested.p
 //need to grab the data from the json files and display them
 
 //displays the data from the json files
-document.addEventListener("DOMContentLoaded", function(){
-   let artistsJson = JSON.parse(artists);
-   let genresJson = JSON.parse(genres);
-   let sampleSongsJson = JSON.parse(sampleSongs);
+document.addEventListener("DOMContentLoaded", async function(){
 
-   //artistsJson.sort((a, b) => b.name.localeCompare(a.name));
+   //let sampleSongsJson = JSON.parse(sampleSongs);
 
+   //api local storage
+   let songStorage = JSON.parse(localStorage.getItem('songs')) || [];
+   songStorage = await getSongData(songStorage);
+   view(songStorage);
+
+   //sort buttons
    document.querySelector("#sortTitle").addEventListener("click", function(){
-      sampleSongsJson = sortItems(sampleSongsJson, 'title');
-      displaySongs(sampleSongsJson, "#sampleSongSelect");
+      songStorage = sortItems(songStorage, 'title');
+      displaySongs(songStorage, "#songBody tbody");
    });
 
    document.querySelector("#sortArtist").addEventListener("click", function(){
-      sampleSongsJson = sortItems(sampleSongsJson, 'artist', 'name');
-      displaySongs(sampleSongsJson, "#sampleSongSelect");
+      sampleSongsJson = sortItems(songStorage, 'artist', 'name');
+      displaySongs(songStorage, "#songBody tbody");
    });
 
    document.querySelector("#sortYear").addEventListener("click", function(){
-      sampleSongsJson = sortItems(sampleSongsJson, 'year');
-      displaySongs(sampleSongsJson, "#sampleSongSelect");
+      sampleSongsJson = sortItems(songStorage, 'year');
+      displaySongs(songStorage, "#songBody tbody");
    });
 
    document.querySelector("#sortGenre").addEventListener("click", function(){
-      sampleSongsJson = sortItems(sampleSongsJson, 'genre', 'name');
-      displaySongs(sampleSongsJson, "#sampleSongSelect");
+      sampleSongsJson = sortItems(songStorage, 'genre', 'name');
+      displaySongs(songStorage, "#songBody tbody");
    });
 
    document.querySelector("#sortPopularity").addEventListener("click", function(){
-      sampleSongsJson = sortItems(sampleSongsJson, 'details', 'popularity');
-      displaySongs(sampleSongsJson, "#sampleSongSelect");
+      sampleSongsJson = sortItems(songStorage, 'details', 'popularity');
+      displaySongs(songStorage, "#songBody tbody");
    });
 
-   //function to display the artists
-   function artist(){
+   //checking if local storage is empty or not
+   async function getSongData(songStorage){
+      if(songStorage.length == 0){
+         songStorage = await getSongAPI();
+         localStorage.setItem('songs', JSON.stringify(songStorage));
+      }
+      return songStorage;
+   }
+
+   //fetching data from api
+   async function getSongAPI(){
+      const response = await fetch(api);
+      const data = await response.json();
+      return data;
+   }
+
+   function view(songStorage){
+      displaySongs(songStorage, "#songBody tbody");
+      songDropdown();
+   }
+    //function to dispaly the sample songs currently
+   function displaySongs(collection, selector){
+      document.querySelector(selector).innerHTML = "";
+      const table = document.querySelector(selector);
+      for(let value of collection){
+         const tr = document.createElement("tr");
+         const title = document.createElement("td");
+         const artist = document.createElement("td");
+         const year = document.createElement("td");
+         const genre = document.createElement("td");
+         const popularity = document.createElement("td");
+
+         title.textContent = value.title;
+         artist.textContent = value.artist.name;
+         year.textContent = value.year;
+         genre.textContent = value.genre.name;
+         popularity.textContent = value.details.popularity;
+
+         tr.appendChild(title);
+         tr.appendChild(artist);
+         tr.appendChild(year);
+         tr.appendChild(genre);
+         tr.appendChild(popularity);
+
+         table.appendChild(tr);
+
+      } 
+   }
+   //
+   function songDropdown(){
+      let artistsJson = JSON.parse(artists);
+      let genresJson = JSON.parse(genres);
+
       const artistSelect = document.querySelector("#artistSelect");
       for (let value of artistsJson) {
         const option = document.createElement("option");
         option.value = value.id;
-        option.textContent = `ID: ${value.id}, Name: ${value.name}, Type: ${value.type}`;
+        option.textContent = value.name;
         artistSelect.appendChild(option);
       }
-   }
 
-   //function to dispaly the genres
-   function genre(){
       const genreSelect = document.querySelector("#genreSelect");
       for (let value of genresJson) {
         const option = document.createElement("option");
         option.value = value.id;
-        option.textContent = `Genres ID: ${value.id}, Genre Name: ${value.name}`;
+        option.textContent = value.name;
         genreSelect.appendChild(option);
       }
-    }
-
-    //function to dispaly the sample songs currently
-   function displaySongs(songs, selector){
-      document.querySelector(selector).innerHTML = "";
-      const song = document.querySelector(selector);
-      for(let value of songs){
-         const option = document.createElement("p");
-         option.setAttribute("value", value.song_id);
-         option.textContent = `Song Name: ${value.title}, Artist Name: ${value.artist.name}, Genre: ${value.genre.name} Year: ${value.year} Popularity: ${value.details.popularity}`;
-         song.appendChild(option);
-      } 
    }
+   //sort method
    //used this to help get an idea for the sort method
    //https://stackoverflow.com/questions/1129216/sort-array-of-objects-by-string-property-value
    function sortItems(song, value, value2){
       song.sort((a,b) =>{
          if(value2){
             if(a[value][value2].toString().toLowerCase() < b[value][value2].toString().toLowerCase()){
-               return -1
+               return -1;
             }
             else if(a[value][value2].toString().toLowerCase() > b[value][value2].toString().toLowerCase()){
-               return 1
+               return 1;
             }
             else{
                return 0;
@@ -97,10 +137,10 @@ document.addEventListener("DOMContentLoaded", function(){
          }
          else{
             if(a[value].toString().toLowerCase() < b[value].toString().toLowerCase()){
-               return -1
+               return -1;
             }
             else if(a[value].toString().toLowerCase() > b[value].toString().toLowerCase()){
-               return 1
+               return 1;
             }
             else{
                return 0;
@@ -111,14 +151,4 @@ document.addEventListener("DOMContentLoaded", function(){
       return song;
    }
 
-   artist();
-   genre();
-   displaySongs(sampleSongsJson, "#sampleSongSelect");
-
 });
-
-/*
-         document.write("Song ID: " + value.song_id + " Title: " + value.title + " Year: " + value.year + " Artist: " + value.artist.name + " Genre: "
-       + value.genre.name + " Popularity: " + value.details.popularity + "</br>");
-*/
-
